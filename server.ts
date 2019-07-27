b2317dbef0fedf83c1372cc326662216934eb432
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-07-26 18:02:51 -0600

    feat: initial commit

---
+++ server.ts
@@ -0,0 +1,54 @@
+/*const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const api = require('./router');*/
+import { express } from "express";
+import { BodyParser } from "body-parser";
+import { path } from "path";
+import { app } from "";
+
+class Server {
+
+  config() {
+    
+  }
+}
+
+/**
+ * Server config
+ */
+function config() {
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+}
+
+/**
+ * Endpoints Config
+ */
+function routerConfig() {
+  app.use(express.static(__dirname + "/dist/verduleriavirtualweb"));
+  app.use('/api', api);
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/dist/verduleriavirtualweb/index.html"));
+  });
+}
+
+// functions invocations
+config();
+routerConfig();
+
+//Set Port
+app.listen(process.env.PORT || 5000);
